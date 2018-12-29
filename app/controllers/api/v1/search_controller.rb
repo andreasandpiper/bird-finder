526373d49b83a0378474e033e28234034d2d2216
Andrea Wayte <awayte@intouchhealth.com>
2018-12-28 16:45:10 -0800

    set up controller

---
+++ app/controllers/api/v1/search_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SearchController < ApplicationController
+  def index
+    render json: birds
+  end
+
+  private
+
+  def birds
+    { birds: [
+      {name: 'bird 1'},
+      {name: 'bird 2'}
+      ]
+    }
+  end
+
+end
