4caeb109282b897b0a08b2cfd45ad16cf27f794a
Andrea Wayte <awayte@intouchhealth.com>
2019-01-06 21:41:29 -0800

    displays most common in order

---
+++ app/javascript/components/BirdItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const BirdItem = (props) => {
+  const data = props.data
+  return (
+    <li>
+      <h2>{ data['comName'] }</h2>
+      <h4>{ data['sciName'] }</h4>
+      <h4>{ data['locName'] }</h4>
+      <h4>{ data['howMany'] }</h4>
+    </li>
+  )
+}
+
+export default BirdItem
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-     resources :search
-     get 'ebird_url' => 'search#ebird_url'
+     get 'birds' => 'search#birds'
     end
   end
 end
