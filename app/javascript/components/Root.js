19af88d70bb8ce016e4934fe67a98cf0f7e681ca
Andrea Wayte <awayte@intouchhealth.com>
2019-03-22 14:25:59 -0700

    remove middleware

--- app/javascript/components/Root.js
+++ app/javascript/components/Root.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import App from './App'
 import reducer from './reducers'
 import { install } from 'redux-loop'
-import ui_promise from './middleware/ui_promise'
 
-const enhancer = compose(
-  applyMiddleware(ui_promise),
-  install()
-);
-
-const store = createStore(reducer, {}, enhancer)
+const store = createStore(reducer, {}, install())
 
 class Root extends React.Component {
   render() {
